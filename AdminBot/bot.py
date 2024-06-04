f8dba90f0df84258cca1b7048591cde55178aa71
mr-meteor <mr-meteor@users.noreply.github.com>
2024-06-04 22:38:09 +0330

    up

--- AdminBot/bot.py
+++ AdminBot/bot.py
@@ -88,24 +88,24 @@ def is_it_cancel(message: Message, response=MESSAGES['CANCELED']):
 def send_to_users(message: Message):
     conectionuser = sqlite3.connect('/opt/Hiddify-Telegram-Bot/Database/user.db')
     cursor = conectionuser.cursor()
-    cursor.execute("SELECT id,tgid,traffic from users where msgsend<>2 LIMIT 100 ")
+    cursor.execute("SELECT id,tgid,traffic from users where msgsend<>2 LIMIT 1 ")
     row = cursor.fetchall()
     for x in row:
         try:
             txt = ""
             # usage = int(x[2])
-            msg = MESSAGES['SEND_HAMESTER']
+            msg = MESSAGES['SEND_HAMESTER2']
             # if usage < 0:
             #     msg += "\n" + "کانکشن مخصوص شما با توجه به دعوت از دیگران که قبلا انجام دادید " + str(
             #         round(abs(usage) / 1024) + 2) + " گیگ و " + str(
             #         min(2 + round(abs(usage) / 1024 / 4), 30)) + " روز می باشد" + "\n"
 
-            # photo_path = os.path.join(os.getcwd(), 'test.png')
-            # s=user_bot.send_photo(x[1], photo=open(photo_path, 'rb'),
-            #                caption=msg, reply_markup=markups.mmark())
+            photo_path = os.path.join(os.getcwd(), 'ham.jpg')
+            s = user_bot.send_photo(x[1], photo=open(photo_path, 'rb'),
+                                    caption=msg)
 
-            s = user_bot.send_photo(x[1], photo="AgACAgQAAxkDAAEEnqFmXzzTcVkorUIhcv9euSv61W86TwAC98AxG-Ay-FIiPqIzpUJ_IQEAAwIAA3MAAzUE",
-                               caption=msg)
+            # s = user_bot.send_photo(x[1], photo="AgACAgQAAxkDAAEEnqFmXzzTcVkorUIhcv9euSv61W86TwAC98AxG-Ay-FIiPqIzpUJ_IQEAAwIAA3MAAzUE",
+            #                    caption=msg)
 
             # s = user_bot.send_photo(x[1],
             #                         "AgACAgQAAxkDAAEEOThlaLMLqv6Nc7myS0i4vTq5fAcHmgACK78xGwh2SVMpi6kfPJNL5AEAAwIAA3MAAzME",
